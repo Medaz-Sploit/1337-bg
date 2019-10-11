dec2d46eb74779a5015233b9ed4318e9aba009be
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2019-10-11 15:34:52 +0200

    Done

--- Projects/libft/ft_memmove.c
+++ Projects/libft/ft_memmove.c
@@ -6,14 +6,14 @@
 /*   By: mazoukni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 16:57:12 by mazoukni          #+#    #+#             */
-/*   Updated: 2019/10/10 23:27:03 by mazoukni         ###   ########.fr       */
+/*   Updated: 2019/10/11 15:31:06 by mazoukni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_memcpy.c"
 
 void	*ft_memmove(void *dst, void *src, int n)
 {
-	if (*((unsigned char*)src) < *((unsigned char*)dst))
+	if (*((unsigned char*)src) < *((unsigned char*)dst) && *((unsigned char*)dst) < *((unsigned char*)src) +n)
 	{
 		while (n > 0)
 		{
