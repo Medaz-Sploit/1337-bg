d51380afd050c3f749258e8a81b93704bb2f44f7
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2019-10-12 23:29:31 +0200

    Not Done Yet

--- Projects/libft/ft_strdup.c
+++ Projects/libft/ft_strdup.c
@@ -6,16 +6,16 @@
 /*   By: mazoukni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 18:47:07 by mazoukni          #+#    #+#             */
-/*   Updated: 2019/10/12 18:59:18 by mazoukni         ###   ########.fr       */
+/*   Updated: 2019/10/12 23:28:46 by mazoukni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
-#include "ft_strlen.c"
 #include "ft_strlcpy.c"
 
 char	*strdup(const char *s1)
 {
 	const char *copy;
-	copy = malloc((ft_strlen(s1) - 1)*sizeof(char));
-	ft_strlcpy((char)copy,(char)s1, sizeof(s1));
+	copy = malloc((ft_strlen((char*)s1) - 1)*sizeof(char));
+	ft_strlcpy((char*)copy,(char*)s1, sizeof(s1));
+	return ((char*)copy);
 }
