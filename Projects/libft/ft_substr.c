3973e1fc26348de01e150f183b612baa167870f0
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2019-10-12 23:34:30 +0200

    Not Done Yet

--- Projects/libft/ft_substr.c
+++ Projects/libft/ft_substr.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mazoukni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/12 23:31:27 by mazoukni          #+#    #+#             */
+/*   Updated: 2019/10/12 23:32:58 by mazoukni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include "ft_strlcpy.c"
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char *sub;
 	
-	sub = (char) malloc((ft_strlen(s) - start)*sizeof(char));
-	ft_strlcpy(sub, (const char)(s + start), len);
+	sub = malloc((ft_strlen((char*)s) - start)*sizeof(char));
+	ft_strlcpy(sub, (char*)(s + start), len);
 	return (sub);
 }
