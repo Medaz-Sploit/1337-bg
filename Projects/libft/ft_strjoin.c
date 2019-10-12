a8a5e7a71187080008654e0b1a574d04efe8a1dd
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2019-10-13 01:19:29 +0200

    Not Done Yet

--- Projects/libft/ft_strjoin.c
+++ Projects/libft/ft_strjoin.c
@@ -1,37 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mazoukni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/11 22:34:04 by mazoukni          #+#    #+#             */
-/*   Updated: 2019/10/13 01:19:05 by mazoukni         ###   ########.fr       */
+/*   Created: 2019/10/12 23:43:06 by mazoukni          #+#    #+#             */
+/*   Updated: 2019/10/12 23:53:26 by mazoukni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char    *ft_strnstr(const char *str, const char *to_find, size_t len)
-{
 
-
-	if (*to_find == '\0')
-		return (str);
-	while (*str != '\0' && len > 0)
-	{
-
-		while (*str == *to_find && len > 0)
-		{
-			to_find++;
-			if (*to_find == '\0')
-			{
-				return (str);
-			}
-			str++;
-			len--;
-
-		}
-		str++;
-		len--;
-	}
-	return (0);
-}
